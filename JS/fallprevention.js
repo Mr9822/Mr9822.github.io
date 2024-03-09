5c18da64c9e88e444e867aee81219d6075c087e1
Mr9822 <116115022+Mr9822@users.noreply.github.com>
2024-03-09 11:20:06 -0400

    Update fallprevention.js

--- JS/fallprevention.js
+++ JS/fallprevention.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const languageSelection = document.getElementById('language-selection');
+    const workshopTitle = document.querySelector('header h1');
+    const sectionTitle1 = document.getElementById('section-title-1');
+    const sectionContent1 = document.getElementById('section-content-1');
+    const sectionTitle2 = document.getElementById('section-title-2');
+    const sectionContent2 = document.getElementById('section-content-2');
+  //const sectionTitle3 = document.getElementById('section-title-3');
     const sectionTitle4 = document.getElementById('section-title-4');
     const sectionContent4 = document.getElementById('section-content-4');
     const disclaimerTitle = document.getElementById('disclaimer-title');
@@ -8,6 +14,25 @@ document.addEventListener('DOMContentLoaded', function () {
     languageSelection.addEventListener('change', function () {
         const selectedLanguage = languageSelection.value;
         if (selectedLanguage === 'fr') {
+            workshopTitle.textContent = 'Atelier d\'Autonomisation Contre l\'Intimidation - Édition Techniques';
+      sectionTitle1.textContent = 'Développez Votre Force Intérieure';
+      sectionContent1.textContent = "Rejoignez-nous dans cet atelier d'autonomisation où vous apprendrez des techniques essentielles de judo, de jiu-jitsu brésilien (BJJ) et de self-défense. Ces arts martiaux ne se limitent pas aux mouvements physiques; ils sont des outils puissants qui peuvent renforcer votre confiance en vous et vous aider à vous défendre contre l'intimidation.";
+
+      sectionTitle2.textContent = 'Les Avantages du Judo, du BJJ et de la Self-Défense';
+      sectionContent2.innerHTML = `
+        <ul class="benefits-list">
+          <li><i class="fas fa-check"></i> Renforcez votre confiance en vous et votre résilience mentale</li>
+          <li><i class="fas fa-check"></i> Développez des compétences pratiques de self-défense pour des situations réelles</li>
+          <li><i class="fas fa-check"></i> Apprenez à utiliser la force de l'adversaire à votre avantage</li>
+          <li><i class="fas fa-check"></i> Améliorez votre condition physique et votre coordination</li>
+          <li><i class="fas fa-check"></i> Cultivez la discipline, le respect et l'auto-discipline</li>
+        </ul>
+      `;
+
+      //sectionTitle3.textContent = 'Témoignages des Participants';
+      //testimonialContent1.textContent = '«...»';
+      //testimonialContent2.textContent = '«...»';
+
             sectionTitle4.textContent = 'Inscrivez-vous dès maintenant !';
             sectionContent4.textContent = "Faites le premier pas vers un mode de vie plus sûr et plus actif!";
 
