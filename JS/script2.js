42454757127596d5ed9ee40c411446fcea8358bf
Mr9822 <116115022+Mr9822@users.noreply.github.com>
2024-03-14 11:10:44 -0300

    Update script2.js

--- JS/script2.js
+++ JS/script2.js
@@ -2,7 +2,7 @@
 const frenchContent = {
   title: "Prévention des chutes et atterrissage en toute sécurité",
   welcomeMessage: "Bienvenue à l'atelier de prévention des chutes et atterrissage en toute sécurité au Restigouche Dojo!",
-  programDescription: "Notre programme de est conçu pour aider les personnes âgées à réduire leur risque de chute et améliorer leur équilibre et leur mobilité, tout en leur apprenant également à tomber correctement pour réduire le risque de blessure. Nos instructeurs expérimentés vous guideront à travers des sessions d'une heure, une fois par semaine, en fournissant des instructions claires et une communauté de soutien pour vous aider à atteindre vos objectifs.",
+  programDescription: "Notre programme est conçu pour aider les personnes âgées à réduire leur risque de chute et à améliorer leur équilibre et leur mobilité, tout en leur apprenant à tomber correctement pour réduire les risques de blessures. Rejoignez-nous pour une séance unique d'environ 45 minutes dirigée par un instructeur expérimenté.",
   enrollMessage: "Inscrivez-vous maintenant et faites le premier pas vers un mode de vie plus sûr et plus actif !",
   //enrollButton: "Inscription",
   disclaimerButton: "Avertissement",
@@ -12,7 +12,7 @@ const frenchContent = {
 const englishContent = {
   title: "Fall prevention and safe landing workshop",
   welcomeMessage: "Welcome to the fall prevention and safe landing workshop at Restigouche Dojo!",
-  programDescription: "Our program is designed to help elderly adults reduce their risk of falling and improve their balance and mobility, while also teaching them how to fall properly to reduce the risk of injury. Our experienced instructors will guide you through 1-hour sessions, once a week, providing clear instructions and a supportive community to help you achieve your goals.",
+  programDescription: "Our program is designed to help senior citizens reduce their risk of falling and improve their balance and mobility, while also teaching them how to fall properly to reduce the risk of injury. Join us for a one time ~45-minutes sessions led by an experienced instructor.",
   enrollMessage: "Enroll now and take the first step towards a safer and more active lifestyle!",
   //enrollButton: "Enroll",
   disclaimerButton: "Disclaimer",
