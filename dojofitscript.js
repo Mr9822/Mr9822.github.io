9dd2a12426982fec6d90ff0cf3a5f54f137ba409
Mr9822 <116115022+Mr9822@users.noreply.github.com>
2025-04-20 18:36:59 -0300

    Update dojofitscript.js

--- dojofitscript.js
+++ dojofitscript.js
@@ -1,6 +1,6 @@
 const frenchContent = {
   title: "Dojo Fit",
-  sectionTitle1: "Camp Dojo Fit de 4 semaines- Ouvert aux membres et aux non-membres <br><em>Commence en mai!</em>",
+  sectionTitle1: "Camp Dojo Fit de 4 semaines <br> Ouvert aux membres et aux non-membres <br><em>Commence en mai!</em>",
   sectionContent1: "Prépare-toi à transpirer, à évoluer et à créer des liens avec ton équipe comme jamais auparavant. Notre camp Dojo Fit de 4 semaines est conçu pour développer l’athlétisme, la discipline et une vraie chimie d’équipe — juste à temps pour l’été.<br><br>Ce programme est rempli de séances d'entraînement stimulantes et amusantes axées sur la force, l’endurance, la mobilité et le mindset. Des exercices de grappling et des circuits au poids du corps à l’entraînement avec des poids et des jeux qui repoussent tes limites, c’est ici que les jeunes athlètes deviennent plus forts — ensemble.<br><br>Dirigée par des entraîneurs expérimentés, chaque séance est conçue pour développer la détermination, améliorer la forme physique et renforcer des valeurs fondamentales comme le respect, l’effort et le travail d’équipe. Que tu te prépares pour un sport ou que tu veuilles simplement te dépasser, ce camp est fait pour toi.",
   sectionTitle2: "Places limitées — Inscris-toi maintenant !",
   sectionContent2: "Fais de cet été ton plus fort à ce jour. Apprends. Transpire. Crée des liens. Répète.",
@@ -10,7 +10,7 @@ const frenchContent = {
 
 const englishContent = {
   title: "Dojo Fit",
-  sectionTitle1: "4-Week Dojo Fit Camp- Open to members and non-members <br><em>Starts this May!</em>",
+  sectionTitle1: "4-Week Dojo Fit Camp <br> Open to members and non-members <br><em>Starts this May!</em>",
   sectionContent1: "Get ready to sweat, grow, and bond with your teammates like never before. Our 4-week Dojo Fit camp is designed to build athleticism, discipline, and real team chemistry — just in time for summer.<br><br>This program is packed with challenging and fun training sessions focused on strength, endurance, mobility, and mindset. From grappling drills and bodyweight circuits to weight training and games that push your limits, this is where young athletes become stronger — together.<br><br>Led by experienced coaches, every session is built around developing grit, improving fitness, and reinforcing core values like respect, effort, and teamwork. Whether you're preparing for sport or just want to level up, this camp is for you.",
   sectionTitle2: "Spots Are Limited — Register Now!",
   sectionContent2: "Make this summer your strongest yet. Learn. Sweat. Bond. Repeat.",
